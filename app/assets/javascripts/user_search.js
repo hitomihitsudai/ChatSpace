cc3e7e6af4527f62284aba5069ed100aacc785cd
hitomihitsudai <murotoshittoro333@gmail.com>
2018-10-10 17:54:14 +0900

    clickイベント：チャットメンバー追加
    （残り）
    追加をclickしたユーザーの消去
    削除をclickしたユーザーをHTMLから取り除く
    Group参加済みメンバーの検索除外
    チャットメンバーを並べて表記

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,5 +1,5 @@
 $(function(){
-  function appendUser(user){
+  function searchUser(user){
 
     var html = `<div class="chat-group-user clearfix">
 					<p class="chat-group-user__name">${user.user_name}</p>
@@ -15,36 +15,43 @@ $(function(){
 	$("#user-search-field").on("keyup", function() {
 	    var input = $("#user-search-field").val();
 	    var href = window.location.href + ''
-
 	    $.ajax({
 	      url: '/users',
 	      type: "GET",
 	      data: { keyword: input },
 	      dataType: 'json',
 	    })
-
 		.done(function(users) {
-		$('#user-search-result').empty();
-			if (users.length !== 0) {
-				users.forEach(function(user){
-				appendUser(user);
-			var html = appendUser(user);
-			$('#user-search-result').append(html)
-			$("#user-search-result").on("click","a",function(){
-			})
-			
-			});
-		 }
+			$('#user-search-result').empty();
+				if (users.length !== 0) {
+					users.forEach(function(user){
+					searchUser(user);
+					var html = searchUser(user);
+					$('#user-search-result').append(html)
+				});
+			 }
 	    })
-
-
-
-
-
-
-
 	    .fail(function() {
     		alert('ユーザー検索に失敗しました');
     	})
 })
+})
+$(function(){
+  function appendUser(user_id, user_name){
+    var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user_id}'>
+					<input name='group[user_ids][]' type='hidden' value= ${user_id}>
+						<p class='chat-group-user__name'>${user_name}</p>
+							<a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+				</div>`
+    return (html);
+  }
+
+
+	$("#user-search-result").on("click",".chat-group-user__btn--add" ,function(){
+		var user_id = $(this).attr("data-user-id");
+		console.log(user_id)
+		var user_name = $(this).attr("data-user-name");
+		var html = appendUser(user_id, user_name);
+		$('#chat-group-users').append(html)
+	})
 })
