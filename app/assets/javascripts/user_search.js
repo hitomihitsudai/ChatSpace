bbe2b879bb4d306676f81ab456b6678bbcf48604
hitomihitsudai <murotoshittoro333@gmail.com>
2018-10-11 11:38:36 +0900

    json.user_id→json.id

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -2,12 +2,12 @@ $(function(){
   function searchUser(user){
 
     var html = `<div class="chat-group-user clearfix">
-					<p class="chat-group-user__name">${user.user_name}</p>
+					<p class="chat-group-user__name">${user.name}</p>
 						<a class="user-search-add
 						 chat-group-user__btn
 						 chat-group-user__btn--add"
-						 data-user-id= ${user.user_id}
-						 data-user-name=${user.user_name}>追加</a>
+						 data-user-id= ${user.id}
+						 data-user-name=${user.name}>追加</a>
 				</div>`
     return (html);
   }
