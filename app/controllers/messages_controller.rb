54c004af761f7c443b36a5868c6d58ccffb2f4cf
hitomihitsudai <murotoshittoro333@gmail.com>
2018-10-14 20:49:00 +0900

    message_controllerインデント修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,18 +5,18 @@ class MessagesController < ApplicationController
 		@message = Message.new
 		@messages = @group.messages.includes(:user)
 		respond_to do |format|
-    	format.html
-    	format.json
-    	end
+		format.html
+		format.json
+		end
 	end
 
 	def create
 		@message = @group.messages.new(message_params)
 
 		if @message.save
-		    respond_to do |format|
-	    	format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
-	    	format.json
+			respond_to do |format|
+			format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+			format.json
 	  	end
 
 		else
