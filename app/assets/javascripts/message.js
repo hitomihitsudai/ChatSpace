ce9a4b267bb2dd36042c78e48a4f5fc9f434d24f
hitomihitsudai <murotoshittoro333@gmail.com>
2018-10-14 11:52:31 +0900

    （自動更新）message.jsに統合

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,7 +5,7 @@ $(function(){
     if (message.image){
       MessageImage = `<img class="lower-message__image" src="${message.image}">`
     }
-    var html = `<div class= message>
+    var html = `<div class= "message" "data-message-id"=${message.id}>
                   <div class= upper-message>
                     <div class= upper-message__user-name>
                       ${message.user_name}
@@ -28,6 +28,7 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var href = window.location.href + ''
+    $('.form__submit').removeAttr('data-disable-with');
 
     $.ajax({
       url: href,
@@ -48,5 +49,33 @@ $(function(){
       alert('非同期通信に失敗しました');
       })
 })
+
+    var interval = setInterval(function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: location.href,
+        type: "GET",
+        dataType: 'json',
+      })
+
+    .done(function(data){
+      var id = $('.message').last().data('messageId');
+      var chatHTML = '';
+      data.forEach(function(message){
+        if (message.id > id){
+        chatHTML += buildHTML(message);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "first");
+        }
+      });
+      $('.messages').append(chatHTML);
+
+    })
+    .fail(function(data){
+      alert('メッセージ取得に失敗しました');
+    });
+} else {
+  clearInterval(interval);
+}} , 5 * 1000 );
+
 })
 })
