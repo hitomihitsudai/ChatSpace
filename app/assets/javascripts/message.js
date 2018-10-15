7d1db59a801626329bc005c98f7cd7e3c0527a2c
hitomihitsudai <murotoshittoro333@gmail.com>
2018-10-15 19:23:43 +0900

    引数の設定変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,27 +1,66 @@
 $(document).on('turbolinks:load', function() {
 $(function(){
+
+  // 画面上の最新ID
+  var latest_id = 0;
+
   function buildHTML(message){
     var MessageImage = ``
     if (message.image){
       MessageImage = `<img class="lower-message__image" src="${message.image}">`
     }
-    var html = `<div class= "message" "data-message-id"=${message.id}>
-                  <div class= upper-message>
-                    <div class= upper-message__user-name>
-                      ${message.user_name}
+    var html = ` <div class= "message" "data-message-id"=${message.id}>
+                    <div class= upper-message>
+                      <div class= upper-message__user-name>
+                        ${message.user_name}
+                      </div>
+                      <div class= upper-message__date>
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class= upper-message__date>
-                      ${message.created_at}
+                    <div class= lower-message>
+                      <div class= lower-message__content>
+                        ${message.content}
+                      </div>
+                        ${MessageImage}
                     </div>
-                  </div>
-                  <div class= lower-message>
-                    <div class= lower-message__content>
-                      ${message.content}
-                    </div>
-                      ${MessageImage}
-                  </div>
                 </div>`
     return html;
+  }
+
+  function reload() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: location.href,
+        type: "GET",
+        dataType: 'json',
+      })
+
+      .done(function(new_messages){
+
+        // 画面初期表示時のみ実行
+        if (latest_id == 0) {
+          latest_id = $('.message').last().data('messageId');
+        }
+        var chatHTML = '';
+        new_messages.forEach(function(message){
+          if (message.id > latest_id){
+            chatHTML += buildHTML(message);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "first");
+
+          // 画面上の最新IDを更新
+          latest_id = message.id;
+          }
+        });
+      $('.messages').append(chatHTML);
+      })
+      .fail(function(new_messages){
+        alert('メッセージ取得に失敗しました');
+      });
+    }
+    else {
+      clearInterval(intarval);
+    }
   }
 
   $('#new_message').on('submit', function(e){
@@ -41,40 +80,19 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html)
+      $('.messages').append(html);
       $('.form__message').val('');
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "first");
+      // 画面上の最新IDを更新
+      latest_id = data.id;
     })
     .fail(function(){
       alert('非同期通信に失敗しました');
-      })
-})
-
-    var interval = setInterval(function() {
-      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-      $.ajax({
-        url: location.href,
-        type: "GET",
-        dataType: 'json',
-      })
-
-    .done(function(new_messages){
-      var id = $('.message').last().data('messageId');
-      var chatHTML = '';
-      data.forEach(function(new_messages){
-        if (message.id > id){
-        chatHTML += buildHTML(message);
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "first");
-        }
-      });
-      $('.messages').append(chatHTML);
-
-    })
-    .fail(function(new_messages){
-      alert('メッセージ取得に失敗しました');
     });
-    } else {
-      clearInterval(interval);
-    }} , 5 * 1000 );
+  })
+
+  var intarval = setInterval(function(){
+    reload();
+  }, 5000 );
   })
 })
